355abbae143d8b14f37166d084e7fd846452543f
liulangx <liulangx@live.com>
2017-11-16 01:35:43 +0800

    headers

---
+++ initialInclude.h
@@ -0,0 +1,12 @@
+#ifndef INITIALINCLUDE_H
+#define INITIALINCLUDE_H
+
+#include <osgViewer/Viewer>
+#include <osg/LineWidth>
+#include <osg/Geode>
+#include <osg/Geometry>
+#include <vector>
+#include <QObject>
+
+
+#endif // INITIALINCLUDE_H
